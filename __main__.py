9a06e87621ccdbc423973c029688e37df2f12b64
Flint Hignett <fhignett@f1r4s1.codam.nl>
2021-05-24 18:33:54 +0200

    stuck

--- __main__.py
+++ __main__.py
@@ -1,16 +1,71 @@
 import pygame
 import sys
 import numpy as np
+from draw import *
 
 FPS = 27
-SCREEN_HEIGHT = 400
-SCREEN_WIDTH = 400
 GRAPH_SIZE = 2
 NAME = "GRAPH"
 RIGHT_CLICK = (False, False, True)
 LEFT_CLICK = (True, False, False)
+SCREEN_HEIGHT = 500
+SCREEN_WIDTH = 500
 MAP = 1
 
+iteration = 0
+
+def draw_line(surface, m=0, b=0):
+	x1 = 0
+	y1 = m * x1 + b
+	x2 = MAP
+	y2 = m * x2 + b
+
+	x1 = np.interp(x1, [0, MAP], [0, SCREEN_WIDTH])
+	y1 = np.interp(y1, [0, MAP], [SCREEN_HEIGHT, 0])
+	x2 = np.interp(x2, [0, MAP], [0, SCREEN_WIDTH])
+	y2 = np.interp(y2, [0, MAP], [SCREEN_HEIGHT, 0])
+
+	pygame.draw.line(surface, "red", (x1, y1), (x2, y2), 1)
+
+def draw_data(surface, data):
+	if len(data) == 0:
+		return
+	for point in data:
+		x, y = point
+		x = np.interp(x, [0, MAP], [0, SCREEN_WIDTH])
+		y = np.interp(y, [0, MAP], [SCREEN_HEIGHT, 0])
+		pygame.draw.circle(surface, "purple", (x, y), 5)
+
+
+def least_squares(data):
+	np_data = np.array(data)
+	X = np_data[:, 0]
+	y = np_data[:, 1]
+	X_mean = np.mean(X)
+	y_mean = np.mean(y)
+
+	numerator, denominator = 0, 0
+	numerator = sum((X - X_mean) * (y - y_mean))
+	denominator = sum((X - X_mean) ** 2)
+
+	m = numerator / denominator
+	b = y_mean - (m * X_mean)
+	return m, b
+
+def gradient_descent(data, m, b, learning_rate=0.02):
+	global iteration
+	iteration += 1
+	np_data = np.array(data)
+	X = np_data[:, 0]
+	y = np_data[:, 1]
+
+	for i in range(len(X)):
+		guess = m * X[i] + b
+		error = y[i] - guess
+
+		m = m + (error * X[i]) * learning_rate
+		b = b + (error) * learning_rate
+	return m, b
 
 def handle_keys(data):
 	for event in pygame.event.get():
@@ -41,61 +96,19 @@ def init_pygame():
 	surface = surface.convert()
 	return surface, screen
 
-def linear_regression(data):
-	np_data = np.array(data)
-	X = np_data[:, 0]
-	y = np_data[:, 1]
-	X_mean = np.mean(X)
-	y_mean = np.mean(y)
-
-	numerator, denominator = 0, 0
-	numerator = sum((X - X_mean) * (y - y_mean))
-	denominator = sum((X - X_mean) ** 2)
-
-	m = numerator / denominator
-	b = y_mean - (m * X_mean)
-	return m, b
-
-def draw_line(surface, m=0, b=0):
-	x1 = 0
-	y1 = m * x1 + b
-	x2 = MAP
-	y2 = m * x2 + b
-
-	x1 = np.interp(x1, [0, MAP], [0, SCREEN_WIDTH])
-	y1 = np.interp(y1, [0, MAP], [SCREEN_HEIGHT, 0])
-	x2 = np.interp(x2, [0, MAP], [0, SCREEN_WIDTH])
-	y2 = np.interp(y2, [0, MAP], [SCREEN_HEIGHT, 0])
-
-	pygame.draw.line(surface, "red", (x1, y1), (x2, y2), 1)
-
-def draw_data(surface, data):
-	if len(data) == 0:
-		return
-	for point in data:
-		x, y = point
-		x = np.interp(x, [0, MAP], [0, SCREEN_WIDTH])
-		y = np.interp(y, [0, MAP], [SCREEN_HEIGHT, 0])
-		pygame.draw.circle(surface, "white", (x, y), 5)
-
-def gradient_descent(data, m, b, learning_rate=0.02):
-	np_data = np.array(data)
-	X = np_data[:, 0]
-	y = np_data[:, 1]
-
-	for i in range(len(X)):
-		guess = m * X[i] + b
-		error = y[i] - guess
-
-		m = m + (error * X[i]) * learning_rate
-		b = b + (error) * learning_rate
-	return m, b
+def display_info(screen, myfont, m, b):
+	text = myfont.render(f"m = {m:.5f} b = {b:.5f}", 1, "white")
+	screen.blit(text, (10,10))
+	text = myfont.render(f"iteration {iteration}", 1, "white")
+	screen.blit(text, (10,25))
+	return text
 
 def main():
 	clock = pygame.time.Clock()
 	surface, screen = init_pygame()
 	data = []
 	m, b = 0, 0
+	myfont = pygame.font.SysFont("arialblack", 15)
 
 	while True:
 		clock.tick(FPS)
@@ -103,10 +116,11 @@ def main():
 		handle_keys(data)
 		draw_data(surface, data)
 		if len(data) > 1:
-			# m, b = linear_regression(data)
-			m, b = gradient_descent(data, m, b, 0.05)
+			# m, b = least_squares(data)
+			m, b = gradient_descent(data, m, b, 0.1)
 			draw_line(surface, m, b)
 		screen.blit(surface, (0,0))
+		display_info(screen, myfont, m, b)
 		pygame.display.update()
 
 if __name__ == "__main__":
